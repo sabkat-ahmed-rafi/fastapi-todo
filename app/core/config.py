5613209fc0a482db991ea7f3de2db7657971e6e9
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2026-07-20 14:01:15 +0600

    feat: set dotenv configuration

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    DEBUG: bool = False
+
+    # Tell Pydantic to read from the .env file
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+settings = Settings()
