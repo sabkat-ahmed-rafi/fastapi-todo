a6c1ea43999093be7ddb9679dc1c3f97e757d1ee
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2026-07-21 19:12:00 +0600

    feat: configure posgresql database with async sqlalchemy orm

---
+++ app/infrastructure/database/__init__.py
@@ -0,0 +1,8 @@
+from .session import get_db
+from .base import Base
+
+
+__all__ = [
+    "get_db",
+    "Base",
+]
---
+++ app/infrastructure/database/session.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import ( 
+    create_async_engine,
+    async_sessionmaker,
+)
+from core.config import settings
+
+
+
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo = True # It logs every sql query of sqlalchemy. Just for development/learning
+)
+
+SessionLocal = async_sessionmaker(
+    bind = engine,
+    expire_on_commit = False
+)
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
+
+async def connect_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(lambda _: None)
+
+async def diconnect_database():
+    await engine.dispose()
