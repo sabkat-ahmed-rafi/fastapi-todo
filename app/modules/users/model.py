af3d863841c4e679b2cd28323fe6e634745262d9
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2026-08-09 18:18:25 +0600

    feat: create the user model

---
+++ app/modules/users/model.py
@@ -0,0 +1,39 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from infrastructure.database import Base, TimestampMixin, SoftDeleteMixin
+
+
+class Users(TimestampMixin, SoftDeleteMixin, Base):
+    ___tablename__ = "users"
+
+    id: Mapped[str] = mapped_column(
+        primary_key=True
+    )
+
+    email: Mapped[str] = mapped_column(
+        unique=True,
+        index=True,
+        nullable=False
+    )
+
+    password_hash: Mapped[str] = mapped_column(
+        nullable=False,
+    )
+
+    first_name: Mapped[str | None] = mapped_column(
+        nullable=True,
+    )
+
+    last_name: Mapped[str | None] = mapped_column(
+        nullable=True,
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        default=True,
+        nullable=True
+    )
+
+    is_verified: Mapped[bool] = mapped_column(
+        default=False,
+        nullable=False
+    )
