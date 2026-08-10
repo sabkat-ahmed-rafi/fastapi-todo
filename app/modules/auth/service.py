6f38d40b27781321dff7f4b0f90f50ddbde6fac3
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2026-08-10 17:05:31 +0600

    refactor: update the register api

--- app/modules/auth/service.py
+++ app/modules/auth/service.py
@@ -1,7 +1,7 @@
 from .schemas import LoginRequest, RegisterRequest, Token
 from .security import encode_access_token
 from users.service import UserService
-from users.schemas import UserCreate
+from users.schemas import UserCreate, UserResponse
 from users.security import verify_password
 from .exceptions import InactiveUser, InvalidCredentials
 
@@ -12,9 +12,8 @@ class AuthService:
         self.user_service = user_service
 
 
-    async def register(self, data: RegisterRequest) -> Token:
-        user = await self.user_service.create_user(UserCreate(**data.model_dump()))
-        return self._create_token(user.id)
+    async def register(self, data: RegisterRequest) -> UserResponse:
+        return await self.user_service.create_user(UserCreate(**data.model_dump()))
 
 
     async def login(self, data: LoginRequest) -> Token:
